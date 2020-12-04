4549b3fa43c7e1380aa63211d6d98f289ac25fdf
ejzp-06 <ejzp06@gmail.com>
2020-12-03 21:01:48 -0600

    Infrastructure creada

---
+++ Lab4/Lab4.Infrastructure/File.cs
@@ -0,0 +1,14 @@
+﻿
+using System.IO;
+using Lab.Core.Interfaces;
+
+namespace Lab4.Infrastructure.Sources
+{
+    public class JsonFileGameSource : IGameSource
+    {
+        public string GetData()
+        {
+            return File.ReadAllText("game.json");
+        }
+    }
+}
